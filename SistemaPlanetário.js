ba9764aa64c69ea6e2b07b16648260f00e73bfaf
Vinícius Bassete <vbassete@gmail.com>
2023-06-22 20:33:14 +0000

    Last Update

--- SistemaPlanetário.js
+++ SistemaPlanetário.js
@@ -5,26 +5,32 @@
 */
 
 import * as THREE from 'three'
-import DadosPlanetas from "./DadosPlanetas.json" assert {type: 'json'}; 
+import DadosPlanetas from "./DadosPlanetas.json" assert {type: 'json'};
+import { GUI } 		from '../../Assets/scripts/three.js/examples/jsm/libs/lil-gui.module.min.js';
 
 let planets = ["Mercurio", "Venus", "Terra", "Marte", "Jupiter", "Saturno", "Urano", "Netuno"]
 var Tempo = 0.0;
-
+var gui = new GUI();
+let vel, comands,fov, camera;
+let RevX = true, RevY=true, RevZ=true;
 function main(){
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 1,2000);
+    scene.background = new THREE.TextureLoader().load("Assets/Texturas/SistemaSolar/2k_stars_milky_way.jpg")
+    
+
+    var camera = new THREE.PerspectiveCamera(110, window.innerWidth / window.innerHeight, 1,2000);
     camera.position.z=6;
+    initGUI(camera);
     //Coloca a luz no centro do sol
     const luz = new THREE.PointLight(0xffffff, 0.8, 500);
     luz.position.set(0,0,0)
     scene.add(luz)
-
     //Cria o sol
     var SolGeometry = new THREE.SphereGeometry(1,32,32);
     var SolMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('Assets/Texturas/SistemaSolar/2k_sun.jpg')});
     var Sol = new THREE.Mesh(SolGeometry, SolMaterial);
-    Sol.name = "Sol"
-    scene.add(Sol)
+    Sol.name = "Sol";
+    scene.add(Sol);
 
     //Cria a Mercurio
     var MercurioGeometry = new THREE.SphereGeometry(0.1,32,32);
@@ -75,7 +81,7 @@ function main(){
     var saturno = new THREE.Mesh(SaturnoGeometry, SaturnoMaterial);
     Saturno.add(saturno)
     var sAnelGeometry = new THREE.RingGeometry(0.5,0.6,32);
-    var sAnelMaterial = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load("Assets/Texturas/SistemaSolar/saturnringcolor.jpg")});
+    var sAnelMaterial = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load("Assets/Texturas/SistemaSolar/2k_saturn_ring_alpha.png")});
     var sAnel = new THREE.Mesh(sAnelGeometry, sAnelMaterial);
     sAnel.rotateX(Math.PI*90/180)
     Saturno.add(sAnel);
@@ -89,7 +95,7 @@ function main(){
     var urano = new THREE.Mesh(UranoGeometry, UranoMaterial);
     Urano.add(urano)
     var uAnelGeometry = new THREE.RingGeometry(0.5,0.55,32);
-    var uAnelMaterial = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load("Assets/Texturas/SistemaSolar/uranusringcolor.jpg")});
+    var uAnelMaterial = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load("Assets/Texturas/SistemaSolar/uranusringcolour.jpg")});
     var uAnel = new THREE.Mesh(uAnelGeometry, uAnelMaterial);
     uAnel.rotateY(Math.PI*90/180);
     Urano.add(uAnel);
@@ -109,6 +115,19 @@ function main(){
     animate(scene,renderer, camera);
 }
 
+function initGUI(camera) {
+
+	vel = 	{VelTempo : 1};
+    fov = {FOV : 110}
+    comands = {RevX:true,RevY:true,RevZ:true};
+	gui.add( vel, 'VelTempo',0,10,0.1);
+    gui.add(fov, "FOV", 50,150,10).onChange(value => camera.fov=(value));
+    gui.add(comands, 'RevX').onChange(value => RevX = !RevX);
+    gui.add(comands, "RevY").onChange(value => RevY = !RevY);;
+    gui.add(comands, "RevZ").onChange(value => RevZ = !RevZ);;
+    gui.open();
+};
+
 //Movimento dos planetas
 function calcMovimento(SemiEixoMaior, Excentricidade, Inclinacao, Periodo, NoLongitude, Periapsis, Tempo){
     var AnomaliaMedia = (2 * Math.PI * Tempo)/Periodo;
@@ -125,16 +144,26 @@ function calcMovimento(SemiEixoMaior, Excentricidade, Inclinacao, Periodo, NoLon
     var sinNo = Math.sin(NoLongitude)
     var cosPeriapsis = Math.cos(Periapsis)
     var sinPeriapsis = Math.sin(Periapsis)
-
-    var xFinal = (cosNo*cosPeriapsis-sinNo*sinPeriapsis*cosInclinacao)*x+(-cosNo*sinPeriapsis-sinNo*cosPeriapsis*cosInclinacao)*y;
+    if(RevX == true){
+        var xFinal = (cosNo*cosPeriapsis-sinNo*sinPeriapsis*cosInclinacao)*x+(-cosNo*sinPeriapsis-sinNo*cosPeriapsis*cosInclinacao)*y;
+    }else{
+        var xFinal = 0;
+    };
+    if(RevY == true){
     var yFinal = (sinNo*cosPeriapsis+cosNo*sinPeriapsis*cosInclinacao)*x+(-sinNo*sinPeriapsis+cosNo*cosPeriapsis*cosInclinacao)*y;
+    }else{
+        var yFinal = 0;
+    };
+    if(RevZ == true){
     var zFinal = (sinPeriapsis*sinInclinacao)* x + (cosPeriapsis*cosPeriapsis)*y;
-
+    }else{
+        var zFinal = 0;
+    };
     return [xFinal, yFinal, zFinal]
 }
 
 function animate(scene,renderer, camera){
-    Tempo += 1;
+    Tempo += 1*vel.VelTempo;
     for(let i = 0; i<=7;i++){
         
         let planeta = planets[i];
@@ -143,12 +172,13 @@ function animate(scene,renderer, camera){
         let fPosition = calcMovimento(1+dados["SemiEixoMaior"], dados["Excentricidade"], 3.1415/180*dados["Inclinacao"],dados["Periodo"], 3.1415/180*dados["NoLongitude"], 3.1415/180*dados["Periapsis"], Tempo);
         curObj.position.set(fPosition[0],fPosition[1],fPosition[2]);
         let Checa0 = (x) => (x === 0 ? 0 : 1 / x);
-        curObj.rotateX(2*Math.PI*Checa0(dados["RotateX"])*Tempo);
-        curObj.rotateY(2*Math.PI*Checa0(dados["RotateY"])*Tempo);
-        curObj.rotateZ(2*Math.PI*Checa0(dados["RotateZ"])*Tempo);
+        curObj.rotateX(2*Math.PI*Checa0(dados["RotateX"])*vel.VelTempo);
+        curObj.rotateY(2*Math.PI*Checa0(dados["RotateY"])*vel.VelTempo);
+        curObj.rotateZ(2*Math.PI*Checa0(dados["RotateZ"])*vel.VelTempo);
     };
     let Sol = scene.getObjectByName("Sol");
     Sol.rotateY(0.001);
+    camera.updateProjectionMatrix();
     requestAnimationFrame(() => animate(scene,renderer, camera));
     renderer.render(scene,camera);
 }
